28e6da21eecc9662abb86603df9dbc421ab99a39
KoenBos <koenbos@outlook.com>
2024-06-27 14:58:56 +0200

    Bug fix: slug already taken bij edditen

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -75,11 +75,19 @@ class PostController extends Controller
     public function update(Request $request, string $id)
     {
         //validate the data
-        $this->validate($request, [
-            'title' => 'required|max:255',
-            'slug' => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
-            'body' => 'required'
-        ]);
+        $post = Post::find($id);
+        if ($request->input('slug') == $post->slug) {
+            $this->validate($request, [
+                'title' => 'required|max:255',
+                'body' => 'required'
+            ]);
+        } else {
+            $this->validate($request, [
+                'title' => 'required|max:255',
+                'slug' => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
+                'body' => 'required'
+            ]);
+        }
 
         //store in the database
         $post = Post::find($id);
